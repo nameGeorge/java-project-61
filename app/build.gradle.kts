8abbf15dc1900bed005311578cd38282814d20fd
nameGeorge <geomel@yandex.ru>
2024-06-28 23:36:35 +0300

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,13 @@ application {
     // Входная точка
     mainClass = "hexlet.code.App"
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
-
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
 tasks.test {
     useJUnitPlatform()
 }
