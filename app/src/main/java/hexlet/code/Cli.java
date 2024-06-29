d2c59d81ebc71bb69dffd2b65e75264ef62737e2
nameGeorge <geomel@yandex.ru>
2024-06-29 12:01:09 +0300

    step

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-
     public static void greet() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nWelcome to the Brain Games!");
