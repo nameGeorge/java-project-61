4684e61a14ed56cad163bc250446e58960c4433e
nameGeorge <geomel@yandex.ru>
2024-07-06 20:22:01 +0300

    step5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greet();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        String userChoice = scanner.next();
+        System.out.println("Your choice: " + userChoice);
+
+        if (userChoice.equals("1")) {
+            Cli.greet();
+        }  else if (userChoice.equals("2")) {
+            Even.isEven();
+        } else {
+            System.exit(0);
+        }
+
+        scanner.close();
     }
 }
 
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,65 @@
+package hexlet.code;
+import java.util.Scanner;
+
+import java.util.Random;
+
+public class Even {
+    private static final int CORRECT_ANSWERS = 3;
+    private static final int RANDOM_RANGE = 100;
+    public static void isEven() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        var sum = 0;
+
+        while (sum < CORRECT_ANSWERS) {
+            Random random = new Random();
+            var randomNumber = random.nextInt(RANDOM_RANGE) + 1;
+            System.out.println("Question: " + randomNumber);
+            String userAnswer = scanner.next();
+            System.out.println("Your answer: "  + userAnswer);
+
+            if (randomNumber % 2 == 0 && userAnswer.equals("yes")
+                    || !(randomNumber % 2 == 0) && userAnswer.equals("no")) {
+                sum = sum + 1;
+                System.out.println("Correct!");
+                if (sum == CORRECT_ANSWERS) {
+                    System.out.println("Congratulations, " + userName + "!");
+                    System.exit(0);
+                }
+
+            } else if (!(randomNumber % 2 == 0)  && userAnswer.equals("yes")) {
+                System.out.println("'" + userAnswer + "'"
+                        + " is wrong answer ;(. Correct answer was " + "'no'."
+                        + "\n Let's try again, " + userName + "!");
+                System.exit(0);
+
+            } else if (randomNumber % 2 == 0 && userAnswer.equals("no")) {
+                System.out.println("'" + userAnswer + "'"
+                        + " is wrong answer ;(. Correct answer was " + "'yes'."
+                        + "\n Let's try again, " + userName + "!");
+                System.exit(0);
+            } else {
+                if (randomNumber % 2 == 0) {
+                    System.out.println("'" + userAnswer + "'"
+                            + " is wrong answer ;(. Correct answer was " + "'yes'."
+                            + "\n Let's try again, " + userName + "!");
+                    System.exit(0);
+                } else {
+                    System.out.println("'" + userAnswer + "'"
+                            + " is wrong answer ;(. Correct answer was " + "'no'."
+                            + "\n Let's try again, " + userName + "!");
+                    System.exit(0);
+                }
+            }
+        }
+
+    }
+}
+
